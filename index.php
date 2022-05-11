75c8547944dcc7b3275b706c939f266a4422339a
bartekxwisn <90174593+bartekxwisn@users.noreply.github.com>
2022-05-11 11:06:18 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title></title>
+        </head>
+            <body>
+            
+            </body>
+
+<?php
+
+$liczba1=$_GET['liczba1'];
+$liczba2=$_GET['liczba2'];
+$liczba3=$_GET['liczba3'];
+$liczba4=$_GET['liczba4'];
+
+$suma1=$liczba1*$liczba2;
+$suma2=$liczba3*$liczba4;
+
+echo $suma1;
+?>
+
+            
+    </html>

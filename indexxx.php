f0fed688e2a71f84c04148ca0a4fa7b647fb8f68
bartekxwisn <90174593+bartekxwisn@users.noreply.github.com>
2022-05-25 08:26:57 +0200

    Add files via upload

---
+++ indexxx.php
@@ -0,0 +1,28 @@
+<!doctype html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title></title>
+        </head>
+            <body>
+            
+            </body>
+
+<?php
+
+$liczba1=$_GET['liczba1'];
+$liczba2=$_GET['liczba2'];
+
+$suma1=$liczba1+$liczba2;
+$suma2=$liczba1-$liczba2;
+$suma3=$liczba2-$liczba1
+$suma4=$liczba1*$liczba2
+$suma5=$liczba1/$liczba2
+$suma5=$liczba2/$liczba1
+echo dodawanie: $suma1 ;
+echo odejmowanie: $suma2 ;
+echo odejmowanie odwrotne: $suma3 ;
+echo mnożenie: $suma4 ;
+echo dzielenie: $suma5 ;
+echo dzielenie odwrotne: $suma6 ;
+?>
